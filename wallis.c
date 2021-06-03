921fff0d0464eafd7629f3f2f8e013db161e56f3
joyaljojo60 <84674969+joyaljojo60@users.noreply.github.com>
2021-06-03 17:27:05 +0000

    Update wallis.c

--- wallis.c
+++ wallis.c
@@ -3,7 +3,18 @@
 #include <stdio.h>
 #include <math.h>
 
-float wallis_pi(int);
+float wallis_pi(int n)
+{
+  float pi;
+  int i;
+  for(i=1;i<=n;i++)
+  {
+    
+  pi*=1/(1-(1/(4*i*i)));
+  }
+  return pi*2;
+}
+  
 
 int main(void) {
   float pi;
